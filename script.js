f08c402dd3880ecb193ed580df933e96437bb4b8
HevA0114 <albaytarheaven@gmail.com>
2024-12-12 18:11:22 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Add a popup functionality
+document.getElementById('popup-button').addEventListener('click', function() {
+    alert(
+        "Pwede ka po bang sumama sakin sa monday after ng class po natin para sa coffee date po, kung oo chat nyo po sakin 'punta tayo sa forever dada' if hindi well oo lang ang choice hehehehe"
+    );
+});
